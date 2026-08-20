142afb360510e39eb3e1b44514e9355d42de3dc0
GuessJeans <lgreenwood@guess.com>
2026-08-20 13:15:07 -0700

    lots of stuff

--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ const startItemSpeed = 5;
 const startPointGoal = 3;
 const startBagSize = 150;
 const startItemSize = 50;
-const startTextH1 = 100;
+const startTextH1 = 80;
 const startTextH2 = 50;
-const startTextH3 = 25;
+const startTextH3 = 35;
 const gameAspect = .5625;
 
 let canvasX = 540;
@@ -37,10 +37,26 @@ let pointGoal = startPointGoal;
 
 let img_bag;
 let img_bg;
+let img_energy;
+let img_phone;
+let c1;
+let c2;
+let f1;
+let f2;
+let f3;
 
 async function setup() {
-  img_bag = await loadImage('img/Bag_A_512x512.png');
-  img_bg = await loadImage('img/bg_540x960.png');
+  img_bag = await loadImage('img/Bag_D_512x512.png');
+  img_bg = await loadImage('img/bg_B_540x960.png');
+  img_energy = await loadImage('img/item_good_energy.png');
+  img_phone = await loadImage('img/item_bad_phone.png');
+  f1 = await loadFont('https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap'); // serif
+  f2 = await loadFont('https://fonts.googleapis.com/css2?family=Playfair+Display:ital@1&display=swap'); // serif italics
+  f3 = await loadFont('https://fonts.googleapis.com/css2?family=Outfit:wght@550&display=swap'); // sans
+  
+  c1 = color(250,245,240); // bg color
+  c2 = color(10,35,60); // text color
+  
   calcCanvas();
   createCanvas(canvasX, canvasY);
   bag = new Bag();
@@ -55,13 +71,40 @@ function draw() {
   handleInput();
 
   if(gameState == 0){
-    background(220);
+    background(c1);
 
-    textSize(textH2); // UI
-    textWeight(3*sc);
-    text("PLAY GAME v2", 25*sc, canvasY/2);
+    noStroke();
+    fill(c2);
+    textSize(textH1); // UI
+    textFont(f1);
+    textAlign(CENTER, CENTER);
+    text("WHAT'S", canvasX/2, canvasY*.15);
+    
+    textSize(textH3); // UI
+    textFont(f2);
+    textAlign(CENTER, CENTER);
+    text("in my", canvasX/2, canvasY*.225);
+    
+    textSize(textH1); // UI
+    textFont(f1);
+    textAlign(CENTER, CENTER);
+    text("ALLEGRA?", canvasX/2, canvasY*.3);
+    
+    imageMode(CENTER);
+    image(img_bag, canvasX/2, canvasY*.55, canvasY*.3, canvasY*.3)
+    
+    rectMode(CENTER);
+    rect(canvasX/2, canvasY*.8, 250*sc, 80*sc, 25*sc);
+    
+    textSize(35*sc);
+    textFont(f3);
+    textAlign(CENTER, CENTER);
+    fill(255);
+    text("PLAY", canvasX/2, canvasY*.8);
+    
   } else if(gameState == 1){ // WHILE IN PLAY
-    image(img_bg, 0, 0, canvasX, canvasY);
+    //image(img_bg, 0, 0, canvasX, canvasY);
+    background(c1);
 
     gameTime = gameTime + deltaTime;
     if(gameTime >= currentItemSpawnTime){
@@ -74,44 +117,55 @@ function draw() {
     for(let i=0; i<items.length; i++){
       items[i].move();
       items[i].show();
-      if(items[i].offScreen()){
+      if(items[i].offScreen() && items[i].isGood){
         gameState++;
       }
       if(items[i].inBag(bagX, bagY)){
-        score++;
-        bag.goodAnim();
-        items.splice(i,1);
-        if (score >= pointGoal){
-          nextLevel();
+        if(items[i].isGood){
+          score++;
+          bag.goodAnim();
+          if (score >= pointGoal){
+            nextLevel();
+          }
+        } else {
+          bag.badAnim();
+          gameState++;
         }
+        items.splice(i,1);
       }
 
     }
 
-    strokeWeight(4*sc);
+    //strokeWeight(4*sc);
     //square(bagX-(bagSize/2), bagY-(bagSize/2), bagSize); // bag
     bag.move();
     bag.show();
 
-    textSize(textH2); // UI
-    strokeWeight(2*sc);
-    text("level " + level, 25*sc, 75*sc);
-    textSize(textH3);
-    strokeWeight(1*sc);
-    text("points: " + score, 25*sc, 120*sc);
+    push();
+    noStroke();
+    fill(c2);
+    textFont(f1);
+    textAlign(CENTER);
+    textSize(textH1); // UI
+    textWeight(1*sc);
+    text(score, canvasX/2, 100*sc);
+    pop();
 
   } else if(gameState == 2){ // ENDING SCREEN
-    background(220,80,80);
-
-    textSize(textH2); // UI
-    strokeWeight(3*sc);
-    text("GAME OVER", 25*sc, 110*sc);
+    background(c2);
+    
+    noStroke();
+    textAlign(CENTER, CENTER);
+    fill(255);
+    
+    stroke(c2);
+    textSize(textH1); // UI
+    textFont(f1);
+    text("GAME OVER", canvasX/2, 110*sc);
     textSize(textH2);
-    strokeWeight(2*sc);
-    text("level " + level, 25*sc, 175*sc);
+    //textFont(f3);
     textSize(textH3);
-    strokeWeight(1*sc);
-    text("points: " + score, 25*sc, 220*sc);
+    text("points: " + score, canvasX/2, 180*sc);
   }
 }
 
@@ -198,6 +252,11 @@ class Item {
   constructor(x,y){
     this.x = x;
     this.y = y;
+    this.isGood = true;
+    let ran = random(1);
+    if (ran > .9){
+      this.isGood = false;
+    }
   }
 
   move(){
@@ -205,9 +264,18 @@ class Item {
   }
 
   show(){
-    stroke(10);
+    push();
+    if(this.isGood){
+      fill(255);
+    } else {
+      fill(255,0,0);
+    }
+    stroke(c2);
     strokeWeight(4*sc);
     ellipse(this.x, this.y, itemSize);
+    //imageMode(CENTER);
+    //image(img_energy, this.x, this.y, itemSize*2, itemSize*2);
+    pop();
   }
 
   inBag(mX,mY){
@@ -242,13 +310,18 @@ class Bag {
     let lastX = this.x;
     let lastSpeed = this.speed;
     this.x = bagX;
-    //this.y = bagY-(bagSize/2);
+    //this.y = bagY;
     this.speed = (this.x - lastX)/100;
     this.r = (this.r + this.speed)*.8;
+    this.r = map(this.r, -1, 1, -1, 1, true);
   }
   
   goodAnim(){
     this.scaleAdd = 20;
+  }
+  
+  badAnim(){
+    console.log("trigger bad anim");
   }
 
   show(){
